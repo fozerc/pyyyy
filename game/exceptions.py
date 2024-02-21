161468d0c07ab99302f64d69d4eceb97f95b1714
fozerc <an07ka@gmail.com>
2024-02-21 13:46:20 +0100

    making game pt2

--- game/exceptions.py
+++ game/exceptions.py
@@ -3,9 +3,22 @@ from settings import ALLOWED_ATTACKS
 class InvalidInput(Exception):
     pass
 
-
 def incorrect_input(input_int):
-    if input_int not in ALLOWED_ATTACKS and \
-       input_int not in ALLOWED_ATTACKS and \
-       input_int not in ALLOWED_ATTACKS:
-       raise InvalidInput("Incorrect input")
+    if input_int not in ALLOWED_ATTACKS:
+        raise InvalidInput("Incorrect input")
+
+
+class GameOver(Exception):
+    pass
+
+def player_dead(player_lives):
+    if player_lives == 0:
+        raise GameOver("You're dead, try to play again!")
+
+
+class EnemyDown(Exception):
+    pass
+
+def enemy_dead(enemy_lives):
+    if enemy_lives == 0:
+        raise EnemyDown("Enemy is dead, you're win!")
--- game/models.py
+++ game/models.py
@@ -1,7 +1,12 @@
 from settings import PLAYER_LIVES
+from settings import ALLOWED_ATTACKS
 from exceptions import InvalidInput
 from exceptions import incorrect_input
-
+from exceptions import GameOver
+from exceptions import player_dead
+from exceptions import EnemyDown
+from exceptions import enemy_dead
+from random import randint
 
 class Player:
     def __init__(self):
@@ -9,17 +14,40 @@ class Player:
         self.lives: int = PLAYER_LIVES
         self.score: int = 0
 
-    def select_attack(self):
+    def select_attack(self) -> str:
         while True:
-            user_input = input("Input enter 1, 2, 3: ")
+            user_input = input("Input 1, 2, 3: ")
             try:
                 incorrect_input(user_input)
-                break
+                return ALLOWED_ATTACKS.get(user_input)
             except InvalidInput:
                 print("Please input numbers: 1, 2, 3")
                 continue
 
-play = Player()
-play.select_attack()
+    def decrease_lives(self):
+        self.lives -= 1
+        print("You got -1 hp")
+        try:
+            player_dead(self.lives)
+        except GameOver:
+            print("You're dead, try to play again!")
 
-# class Enemy:
+    def add_score(self):
+        raise NotImplementedError
+
+
+class Enemy:
+    def __init__(self):
+        self.lives: int = 1
+        self.level: int = 1
+
+    def select_attack(self):
+        random_attack = str(randint(1, 3))
+        return ALLOWED_ATTACKS.get(random_attack)
+
+    def decrease_lives(self):
+        self.lives -= 1
+        try:
+            enemy_dead(self.lives)
+        except EnemyDown:
+            print("Enemy is dead, you're win!")
